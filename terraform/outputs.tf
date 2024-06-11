f64eef188196acfe3f5dd8fbe1c2147295fa6c06
urssaibalaji <balajisai1406@gmail.com>
2024-06-11 11:55:22 +0530

    balajicommit55

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,3 @@
-output "cluster_name" {
-  value = aws_ecs_cluster.cluster.name
-}
-
-output "service_name" {
-  value = aws_ecs_service.service.name
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.main.name
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,21 +1,3 @@
-variable "task_execution_role_arn" {
-  description = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  type        = string
-}
-
-variable "image_url" {
-  description = "urssaibalaji/hello-world-nodejs:latest"
-  type        = string
-}
-
-variable "cluster_name" {
-  description = "hello-world-cluster"
-  type        = string
-  default     = "my-cluster"
-}
-
-variable "service_name" {
-  description = "hello-world-service"
-  type        = string
-  default     = "my-service"
+variable "aws_region" {
+  default = "us-west-2"
 }
