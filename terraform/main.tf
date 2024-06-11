b4f0d119c3068d75377995b554b0151d0f5e1a00
urssaibalaji <balajisai1406@gmail.com>
2024-06-11 12:08:26 +0530

    balajicommitnodejs

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,66 +1,41 @@
-provider "aws" {
-  region = "us-west-2"
+resource "aws_ecs_cluster" "cluster" {
+  name = var.ecs_cluster_name
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
-}
-
-resource "aws_security_group" "main" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_ecs_cluster" "main" {
-  name = "hello-world-cluster"
-}
-
-resource "aws_ecs_task_definition" "hello_world" {
+resource "aws_ecs_task_definition" "task" {
   family                   = "hello-world-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = 256
-  memory                   = 512
+  cpu                      = "256"
+  memory                   = "512"
 
-  container_definitions = jsonencode([{
-    name  = "hello-world"
-    image = "your-dockerhub-username/hello-world:latest"
-    essential = true
+  execution_role_arn = var.task_execution_role_arn
 
-    portMappings = [{
-      containerPort = 3000
-      hostPort      = 80
-    }]
-  }])
+  container_definitions = jsonencode([
+    {
+      name      = "hello-world-container"
+      image     = "your-dockerhub-username/hello-world-nodejs:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
 }
 
-resource "aws_ecs_service" "main" {
-  name            = "hello-world-service"
-  cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.hello_world.arn
+resource "aws_ecs_service" "service" {
+  name            = var.service_name
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
   desired_count   = 1
 
+  launch_type = "FARGATE"
+
   network_configuration {
-    subnets         = [aws_subnet.main.id]
-    security_groups = [aws_security_group.main.id]
+    subnets         = ["subnet-0d32508389535e712", "subnet-0f380a26b9b500d76"]
+    security_groups = ["sg-019423dc02284f052"]
   }
 }
