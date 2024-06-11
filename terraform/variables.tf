d414cd3355c6f4c94c22142061b0d8a6d2a5d41d
urssaibalaji <balajisai1406@gmail.com>
2024-06-11 12:11:26 +0530

    balajicommit111

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,15 @@
 variable "aws_region" {
-  default = "us-west-2"
+  default = "us-east-1"
+}
+
+variable "ecs_cluster_name" {
+  default = "hello-world-cluster"
+}
+
+variable "service_name" {
+  default = "hello-world-service"
+}
+
+variable "task_execution_role_arn" {
+  description = "The ARN of the task execution role"
 }
