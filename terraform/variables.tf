d7d827e3d0cc7af0ab1dc54261b6f88b7418337b
urssaibalaji <balajisai1406@gmail.com>
2024-06-11 11:51:48 +0530

    balajicommit1401

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,21 @@
+variable "task_execution_role_arn" {
+  description = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  type        = string
+}
+
 variable "image_url" {
   description = "urssaibalaji/hello-world-nodejs:latest"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "hello-world-cluster"
+  type        = string
+  default     = "my-cluster"
+}
+
+variable "service_name" {
+  description = "hello-world-service"
+  type        = string
+  default     = "my-service"
 }
